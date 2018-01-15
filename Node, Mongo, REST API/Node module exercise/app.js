dcfcb74a62241c02af0ad4365292d1a7463d128a
Mattemyo <matias97marti@gmail.com>
2018-01-15 21:16:46 +0100

    import module

---
+++ Node, Mongo, REST API/Node module exercise/app.js
@@ -0,0 +1,5 @@
+const calc = require("./calculator");
+const colors = require("colors");
+
+console.log(calc.add(5, 5));
+console.log("hej".green);
---
+++ Node, Mongo, REST API/Node module exercise/calculator.js
@@ -0,0 +1,9 @@
+const calc = {
+  add: (a, b) => a + b,
+  sub: (a, b) => a - b,
+  mult: (a, b) => a * b,
+  div: (a, b) => a / b
+};
+
+module.exports = calc;
+
