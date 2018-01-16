de0fa4f11a7d70160e2697daf02de4ecf9d21f38
Mattemyo <matias97marti@gmail.com>
2018-01-16 18:20:14 +0100

    start with get and put requests

--- Node, Mongo, REST API/First-API/server.js
+++ Node, Mongo, REST API/First-API/server.js
@@ -1,15 +1,42 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const ingredients = [
+  {
+    id: "232kAk",
+    text: "Eggs"
+  },
+  {
+    id: "t86u",
+    text: "Milk"
+  },
+  {
+    id: "2d8sf97",
+    text: "Bacon"
+  },
+  {
+    id: "2d8sf97",
+    text: "Bacon"
+  }
+];
 
 app.get("/", (req, res) => {
-  res.send("my first api");
-});
-app.get("/funions", (req, res) => {
-  res.send("funions");
+  res.send(ingredients);
 });
 
-
+app.post("/", (req, res) => {
+  const ingredient = req.body;
+  if (!ingredient || !ingredient.text) {
+    return res.status(500).send({ error: "must have a text" });
+  }
+  ingredients.push(ingredient);
+  res.status(200).send(ingredients);
+});
 
 app.listen(3000, () => {
-    console.log('App listening on port 3000!');
+  console.log("App listening on port 3000!");
 });
