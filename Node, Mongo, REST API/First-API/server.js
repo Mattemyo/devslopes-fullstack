17047d50b20042aeb0940be1ca3cdaed972d1860
Mattemyo <matias97marti@gmail.com>
2018-01-16 18:44:48 +0100

    handle put requests

--- Node, Mongo, REST API/First-API/server.js
+++ Node, Mongo, REST API/First-API/server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const ingredients = [
   {
@@ -19,22 +19,43 @@ const ingredients = [
     text: "Bacon"
   },
   {
-    id: "2d8sf97",
+    id: "dffdfdfe4545",
     text: "Bacon"
   }
 ];
 
-app.get("/", (req, res) => {
+app.get("/ingredients", (req, res) => {
   res.send(ingredients);
 });
 
-app.post("/", (req, res) => {
+app.post("/ingredients", (req, res) => {
   const ingredient = req.body;
   if (!ingredient || !ingredient.text) {
     return res.status(500).send({ error: "must have a text" });
   }
   ingredients.push(ingredient);
   res.status(200).send(ingredients);
+});
+
+app.put("/ingredients/:ingredientId", (req, res) => {
+  const newText = req.body.text;
+  if (!newText || newText == "") {
+    return res.send({ error: "Must provide text" });
+  }
+  let objectFound = false;
+  for (let i = 0; i < ingredients.length; i++) {
+    const ing = ingredients[i];
+
+    if (ing.id === req.params.ingredientId) {
+      ing.text = newText;
+      objectFound = true;
+      break;
+    }
+  }
+  if (!objectFound) {
+    return res.send({ error: "ingredient id not found" });
+  }
+  res.send(ingredients);
 });
 
 app.listen(3000, () => {
