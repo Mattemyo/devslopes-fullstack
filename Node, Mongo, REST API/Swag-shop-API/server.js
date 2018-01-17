750fcb1fd34a8406c88b1f933f7804d941b71e32
Mattemyo <matias97marti@gmail.com>
2018-01-17 18:41:04 +0100

    finish routing and associating wishlist with products

--- Node, Mongo, REST API/Swag-shop-API/server.js
+++ Node, Mongo, REST API/Swag-shop-API/server.js
@@ -1,87 +1,86 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/swag-shop');
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const db = mongoose.connect("mongodb://localhost/swag-shop");
 
-var Product = require('./model/product');
-var WishList = require('./model/wishlist');
-
-//Allow all requests from all domains & localhost
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "POST, GET");
-  next();
-});
-
+// models
+const Product = require("./model/product");
+const WishList = require("./model/wishlist");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/product', function(request, response) {
-    var product = new Product();
-    product.title = request.body.title;
-    product.price = request.body.price;
-    product.save(function(err, savedProduct) {
-       if (err) {
-           response.status(500).send({error:"Could not save product"});
-       } else {
-           response.send(savedProduct);
-       }
+// products routes
+app.post("/product", (req, res) => {
+  const product = new Product();
+  product.title = req.body.title;
+  product.price = req.body.price;
+  product.save((err, savedProduct) => {
+    if (err) {
+      res.status(500).send({ error: "Could not save product" });
+    } else {
+      res.send(savedProduct);
+    }
+  });
+});
+
+app.get("/product", (req, res) => {
+  Product.find({}, (err, products) => {
+    if (err) {
+      res.status(500).send({ error: "Could not fetch products" });
+    } else {
+      res.send(products);
+    }
+  });
+});
+
+// wishlist routes
+app.get("/wishlist", (req, res) => {
+  WishList.find({})
+    .populate({ path: "products", model: "Product" })
+    .exec((err, wishLists) => {
+      if (err) {
+        res.status(500).send({ error: "Could not fetch wishlists" });
+      } else {
+        res.status(200).send(wishLists);
+      }
     });
 });
 
-app.get('/product', function(request, response) {
+app.post("/wishlist", (req, res) => {
+  const wishList = new WishList();
+  wishList.title = req.body.title;
 
-    Product.find({},function(err, products) {
-        if (err) {
-            response.status(500).send({error: "Could not fetch products"});
-        } else {
-            response.send(products);
+  wishList.save((err, newWishList) => {
+    if (err) {
+      res.status(500).send({ error: "Could not create wishlist" });
+    } else {
+      res.send(newWishList);
+    }
+  });
+});
+
+app.put("/wishlist/product/add", (req, res) => {
+  Product.findOne({ _id: req.body.productId }, (err, product) => {
+    if (err) {
+      res.status(500).send({ error: "Could not add item to wishlist" });
+    } else {
+      WishList.update(
+        { _id: req.body.wishListId },
+        { $addToSet: { products: product._id } },
+        (err, wishList) => {
+          if (err) {
+            res.status(500).send({ error: "Could not add item to wishlist" });
+          } else {
+            res.send(wishList);
+          }
         }
-    });
+      );
+    }
+  });
 });
 
-app.get('/wishlist', function(request, response) {
-   WishList.find({}).populate({path:'products', model: 'Product'}).exec(function(err, wishLists) {
-       if (err) {
-           response.status(500).send({error:"Could not fetch wishlists"});
-       } else {
-           response.status(200).send(wishLists);
-       }
-   })
-});
-
-app.post('/wishlist', function(request, response) {
-    var wishList = new WishList();
-    wishList.title = request.body.title;
-
-    wishList.save(function(err, newWishList) {
-       if (err) {
-           response.status(500).send({error: "Could not create wishlist"});
-       } else {
-           response.send(newWishList);
-       }
-    });
-});
-
-app.put('/wishlist/product/add', function(request, response) {
-   Product.findOne({_id: request.body.productId}, function(err, product) {
-       if (err) {
-           response.status(500).send({error:"Could not add item to wishlist"});
-       } else {
-           WishList.update({_id:request.body.wishListId}, {$addToSet:{products: product._id}}, function(err, wishList) {
-               if (err) {
-                   response.status(500).send({error:"Could not add item to wishlist"});
-               } else {
-                   response.send("Successfully added to wishlist");
-               }
-           });
-       }
-   })
-});
-
-app.listen(3004, function() {
-    console.log("Swag Shop API running on port 3004...");
+app.listen(3000, () => {
+  console.log("Swag Shop API running on port 3000...");
 });
